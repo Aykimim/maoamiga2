f7c30bf2304c8ac83c2e4f23f2805d6dcffde2c3
Aykimim <91210511+Aykimim@users.noreply.github.com>
2022-11-25 09:53:41 -0300

    base

--- politica.php
+++ politica.php
@@ -10,15 +10,34 @@
      <nav>
 		
 		<?php include 'header.php'; ?>
-		<h1>Aviso do site ao (a) visitante:<\br><\br>
+		<html>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+<!-- Conteúdo -->
+<h1>Aviso do site ao (a) visitante:</br></br>
 
-Ao ligar, informe que viu o profissional(a) no site Mão Amiga . Eles querem saber! <\br>
-Confirme também a rua e o número, pois o endereço anunciado nunca é o exato, mas apenas uma referência próxima.<\br>
-Não nos envolvemos em tratativas de anunciantes e seus clientes, nem nos responsabilizamos por pagamentos antecipados!<\br>
-Todo atendimento esperado deve ser alinhado com o anunciante antes do encontro.<\br>
-Somente faça o contato via WhatsApp se o intuito for de realmente sair com o anunciante,<\br>
-assim, desocupados com interesse em apenas puxar conversa, serão bloqueados.</h1>
-		<!-- Conteúdo -->
+Ao ligar, informe que viu o profissional(a) no site Mão Amiga . Eles querem saber! 
+</br>
+Confirme também a rua e o número, pois o endereço anunciado nunca é o exato, mas apenas uma referência próxima.</br>
+Não nos envolvemos em tratativas de anunciantes e seus clientes, nem nos responsabilizamos por pagamentos antecipados!</br>
+Todo atendimento esperado deve ser alinhado com o anunciante antes do encontro.
+</br>
+Somente faça o contato via WhatsApp se o intuito for de realmente sair com o anunciante,
+</br>
+assim, desocupados com interesse em apenas puxar conversa, serão bloqueados.
+</br>
+</h1>
+
+
+
+		
+</html>
+		
 	<?php include 'footer.php'; ?>
      </nav>
 
