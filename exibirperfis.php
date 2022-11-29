927fe9cc96e40c4cbece50300348fb0478482b92
Aykimim <91210511+Aykimim@users.noreply.github.com>
2022-11-29 12:45:11 -0300

    foi

--- exibirperfis.php
+++ exibirperfis.php
@@ -1,3 +1,31 @@
+<!-- Olá, Eykimim -->
+<!--///////////////////////////////////////////diretriz/////////////////////////////////////////////////-->
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+     <meta charset="utf-8">
+     <title>Mão amiga</title>
+	 <style rel='stylesheet' type='text/css'>
+
+	</style>
+	<link href='style.css' rel='stylesheet' type='text/css' />
+</head>
+<body>
+
+
+     <nav>
+		
+		<?php include 'header.php'; ?>
+		<html>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+</br>
+<!-- Conteúdo -->
+
 <?php
 // Seleciona todos os usuários
 $sql = mysql_query("SELECT * FROM id_usuario ORDER BY nome_exibido");
@@ -10,3 +38,11 @@ while ($usuario = mysql_fetch_object($sql)) {
     echo "<b>Email:</b> " . $usuario->email . "<br /><br />";
 }
 ?>
+
+</html>
+		
+	<?php include 'footer.php'; ?>
+     </nav>
+
+</body>
+</html>
