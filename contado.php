5df405a935433e12c63e57ab208b799e1f19481d
Aykimim <91210511+Aykimim@users.noreply.github.com>
2022-11-30 12:31:18 -0300

    pedro

--- contado.php
+++ contado.php
@@ -6,6 +6,8 @@
 <head>
      <meta charset="utf-8">
      <title>Mão amiga</title>
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 	 <style rel='stylesheet' type='text/css'>
 
 	</style>
@@ -33,18 +35,15 @@ Contato</br>
 Autores: Eykimim Aniceto Pereira & YASMIM DUTRA COIMBRA .</br>
 <div class="coluna">
 				
-					<img src="img/eykimim.jpg" alt="descrição da imagem" BORDER=5 style="width:100%">
+					<img src="img/eykimim.jpg" alt="descrição da imagem"height="100" width="100" BORDER=5 style="width:100%">
 				</div>
 				<div class="coluna">
-					<img src="img/yasmim.jpg" alt="descrição da imagem" BORDER=5 style="width:100%">
+					<img src="img/yasmim.jpg" alt="descrição da imagem" height="100" width="100"BORDER=5 style="width:100%">
 				</div>
 			</div>
 
 Para entrar em contato preencher o formulario</br>
 </h5>
-
-
-
 
 
 	<form action="enviar-email.php" method="post">
