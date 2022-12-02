2d1f7fb2218f390def178fa2a9fabfbf25675e84
Aykimim <91210511+Aykimim@users.noreply.github.com>
2022-12-02 01:00:42 -0300

    foi

--- contato.php
+++ contato.php
@@ -1,20 +1,6 @@
-<!-- Olá, Eykimim -->
-<!--///////////////////////////////////////////diretriz/////////////////////////////////////////////////-->
-
-<!DOCTYPE HTML>
-<html lang="pt-br">
-<head>
-     <meta charset="utf-8">
-     <title>Mão amiga</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
-	 <style rel='stylesheet' type='text/css'>
-
-	</style>
-	<link href='style.css' rel='stylesheet' type='text/css' />
-</head>
-
-
+<!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">-->
+<!--<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>-->
+	
 <?php include 'header.php'; ?>
 <!-- Conteúdo -->
 <html>
@@ -23,20 +9,19 @@
     Contato</br>
 
 Autores: Eykimim Aniceto Pereira & YASMIM DUTRA COIMBRA .</br>
-<div class="coluna">
+<div class="contato">
 				
-					<img src="img/eykimim.jpg" alt="descrição da imagem"height="100" width="100" BORDER=5 style="width:100%">
-				</div>
-				<div class="coluna">
-					<img src="img/yasmim.jpg" alt="descrição da imagem" height="100" width="100"BORDER=5 style="width:100%">
-				</div>
+					<img src="img/eykimim.jpg" alt="descrição da imagem" height="42" width="42" vspace="5" hspace="5">
+				
+					<img src="img/yasmim.jpg" alt="descrição da imagem" height="42" width="42" vspace="5" hspace="5">
+				
 			</div>
 
 Para entrar em contato preencher o formulario</br>
 </h5>
 
 
-	<form method="post" action="#" >
+	<form method="post" action="enviar_email.php" >
 		  <input type="hidden" name="subject" 
   value="eykimim@hotmail.com" />
     <div>
@@ -56,28 +41,10 @@ Para entrar em contato preencher o formulario</br>
         <textarea id="mensagem" name="mensagem" rows="6" cols="25"></textarea>
     </div>
 
-       <input type="submit" value="Enviar">
+       <input type="submit" value="Enviar" name="botao">
        <input type="reset" value="Cancelar">
 
 	</form>
-</body>
-<?php
-$nome= $_POST['nome'];
-$email = $_POST['email'];
-$mensagem= $_POST['mensagem'];
-$formcontent="De: $email \nMensagem enviada por: $nome \nReclamação: $mensagem ";
-$recipient = "kimmim@kimmim.shop";
-$subject = "Reclamação do mao amiga";
-$mailheader = "De: $email";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Erro!");
-
-function_alert("Mensagem enviada com sucesso");
-
-function function_alert($msg) {
-    echo "<script type='text/javascript'>alert('$msg');</script>";
-}
-
-?>
 
 
 </html>
--- enviar_email.php
+++ enviar_email.php
@@ -1,7 +1,29 @@
 <?php include 'header.php'; ?>
 <!-- Conteúdo -->
 <html>
+<?php
+    if (isset($_POST[“botao”]))
+{
+echo “botão foi clicado”;
 
+
+$nome= $_POST['nome'];
+$email = $_POST['email'];
+$mensagem= $_POST['mensagem'];
+$formcontent="De: $email \nMensagem enviada por: $nome \nReclamação: $mensagem ";
+$recipient = "kimmim@kimmim.shop";
+$subject = "Reclamação do mao amiga";
+$mailheader = "De: $email";
+mail($recipient, $subject, $formcontent, $mailheader) or die("Erro!");
+
+function_alert("Mensagem enviada com sucesso");
+
+function function_alert($msg) {
+    echo "<script type='text/javascript'>alert('$msg');</script>";
+}
+ 
+}
+?>  
 
 
 
