dd1ab0a0ee5d349be2625596058d2f8864cab2ed
Aykimim <91210511+Aykimim@users.noreply.github.com>
2022-12-06 10:35:43 -0300

    a

--- header.php
+++ header.php
@@ -1,22 +1,24 @@
+<!-- Olá, Eykimim -->
 <!--///////////////////////////////////////////diretriz/////////////////////////////////////////////////-->
 <!DOCTYPE HTML>
 <html lang="pt-br">
-	<!-- Olá, Eykimim -->
+	
 <head>
      <meta charset="utf-8">
      <title>Mao amiga</title>
 	 <style rel='stylesheet' type='text/css'>
 	</style>
-	 link rel="icon" type="imagem/png" href="img/favicon-16x16.png">
-	<link href='style.css' rel='stylesheet' type='text/css' />
+	<!-- <link rel="icon" type="imagem/png" href="img/favicon-16x16.png">-->
+	<link href='css/style.css' rel='stylesheet' type='text/css' />
+	<body style= " background-color : white;" >
 	<!--
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
      -->
 </head>
-<body background="white">
+<body>
+<!--<body background="white">-->
 
-<!--///////////////////////////////////////////////////////////////////////////////////////-->
 
 <!--/////////////////////////////////////////cabecario//////////////////////////////////////////////-->
 	
